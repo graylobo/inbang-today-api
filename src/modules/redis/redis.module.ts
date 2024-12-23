b904b0d6b603a7713b1199588105aeda698e24e4
graylobo <cazador.min@gmail.com>
2024-12-23 13:10:58 +0900

    log

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -2,15 +2,16 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { redisStore } from 'cache-manager-redis-store';
 import { RedisService } from 'src/modules/redis/redis.service';
-
+console.log('process.env.REDIS_HOST:', process.env.REDIS_HOST);
+console.log('process.env.REDIS_PORT:', process.env.REDIS_PORT);
 @Module({
   imports: [
     CacheModule.register({
       store: (): any => {
         return redisStore({
           socket: {
-            host: 'localhost',
-            port: 6379,
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT),
           },
           ttl: 60,
         });
