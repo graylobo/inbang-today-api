505c36ce1075c8ab30d79932bd98cc8caa46a083
graylobo <cazador.min@gmail.com>
2025-04-17 05:46:27 +0900

    chore: board image upload s3 prefix

--- src/modules/editor/services/editor.service.ts
+++ src/modules/editor/services/editor.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { generateS3ObjectName } from 'src/modules/aws/utils';
 import { S3Service } from '../../aws/services/s3/s3.service';
 import { Cron } from '@nestjs/schedule';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class EditorService {
-  constructor(private readonly s3Service: S3Service) {}
+  constructor(
+    private readonly s3Service: S3Service,
+    private readonly configService: ConfigService,
+  ) {}
 
   async uploadImage(file: Express.Multer.File) {
     const fileName = generateS3ObjectName(file.originalname);
+    const bucketName = this.configService.get<string>('aws.s3BucketName');
 
     try {
       const url = await this.s3Service.uploadFile({
         file,
-        bucketName: 'artst-app',
-        key: `temp/${fileName}`,
+        bucketName,
+        key: `board-post/${fileName}`,
       });
 
       return {
@@ -28,9 +34,10 @@ export class EditorService {
   @Cron('0 0 * * *')
   async cleanupTempFiles() {
     try {
+      const bucketName = this.configService.get<string>('aws.s3BucketName');
       await this.s3Service.deleteOldObjects({
-        bucketName: 'artst-app',
-        prefix: 'temp/',
+        bucketName,
+        prefix: 'board-post/',
         olderThan: 24 * 60 * 60 * 1000,
       });
       console.log('Temp files cleanup completed');
